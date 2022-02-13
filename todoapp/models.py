83933dc24bd351bdc7f87189d058f64f38745fc8
ankitpakhale <akp3067@gmail.com>
2022-02-13 14:39:11 +0530

    auth commit

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,8 +2,21 @@ from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class signUp(models.Model):
+    name = models.CharField(max_length=30, default='')
+    email = models.EmailField(default='')
+    number = models.PositiveIntegerField(default='')
+    password = models.CharField(default='', max_length=15)
+    confirmPassword = models.CharField(default='', max_length=15)
+    
+    def __str__(self):
+        return self.name
+    
+
 class TodoList(models.Model):
     title =             models.CharField(max_length=100, default='')
     description =       models.TextField(max_length=1000, default='')
     def __str__(self):
         return str(self.title)
+    
--- todoapp/views.py
+++ todoapp/views.py
@@ -6,3 +6,66 @@ from django.http import HttpResponse
 def test(request):
     return HttpResponse("Working properly..............")
 
+def SignupView(request):
+    if request.POST:
+       
+        Name = request.POST['name']
+        Email = request.POST['email']
+        
+        Password = request.POST['password']
+        ConfirmPassword = request.POST['confirmPassword']
+        try:
+            data = UserDetails2.objects.filter(email=Email)
+            if data:
+                msg = "Email already registered"
+                return render(request, 'signup.html', {'msg': msg})
+            elif ConfirmPassword == Password:
+                v = UserDetails2()
+                v.name = Name
+                v.email = Email
+                
+                v.password = Password
+                v.save()
+                print(f"{v.name} Signed up successfully")
+                
+                # getting IPV6 and FE80 address
+                # print(netifaces.interfaces())
+                # addrs = netifaces.ifaddresses('eno1')
+                # # print(addrs)
+                # x = addrs.get(10)
+                # # (x[0])
+                # global_add = x[0].get('addr')
+                # print(global_add)
+                # link_local = x[2].get('addr')
+                # link_local = link_local.replace("%eno1","" )
+                # print(link_local)
+                # i.ipaddress = global_add+':'+link_local
+                IPV6 = requests.get("https://api6.ipify.org", timeout=5).text
+                i = IP()
+                i.ipaddress = IPV6
+                i.save()
+                # return HttpResponse(global_add, link_local)
+
+                return redirect('LOGIN2')
+            else:
+                msg = 'Please Enter Same Password'
+                return render(request , 'signup.html',{'msg':msg}) 
+        finally:
+            messages.success(request, 'Signup Successfully Done...')
+    return render(request,'signup.html')
+
+def userLogin(request):
+    if request.POST:
+        em = request.POST.get('email')
+        pass1 = request.POST.get('password')
+      
+        print("Inside first try block", em)
+        check = UserDetails2.objects.get(email = em)
+        print("Email is ",em)
+        if check.password == pass1:
+            request.session['email'] = check.email
+            print(f'{check.name} Successfully logged in')
+            return redirect('DASHBOARD2')
+        else:
+            return HttpResponse('Invalid Password')
+    return render(request,'login.html')
