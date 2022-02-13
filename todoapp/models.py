d24a4179269e8244a2ecf4b5227acf96126f65d3
ankitpakhale <akp3067@gmail.com>
2022-02-13 14:24:08 +0530

    model commit

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,4 +1,9 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
-
+class TodoList(models.Model):
+    title =             models.CharField(max_length=100, default='')
+    description =       models.TextField(max_length=1000, default='')
+    def __str__(self):
+        return str(self.title)
